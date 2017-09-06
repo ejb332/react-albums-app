77ac03a456fa02b7beccf4884cdbefa346a4d406
Eric Biel <eric.j.biel@gmail.com>
2017-09-06 10:49:22 -0500

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+  // use view tag to wrap all other tags
+  // can only return one top-level tag
+  // flex 1 expands component to fill entire content area of device
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+// Only the root component uses "AppRegistry"
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+// make scrollable
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  //initial state for albums is an empty array
+  // we can now reference this.state.albums in the render function
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    //pass a promise when http request is complete
+    //update the state to update the albums object to include response
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      //each component inside the array must have a key property
+      //how React keeps track of re-rerenders and only updates the correct key
+      <AlbumDetail key={album.title} album={album} />
+      );
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      // wrap all list items to make scrollable
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
+
+export default AlbumList;
